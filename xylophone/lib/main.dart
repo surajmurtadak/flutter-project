8bb406bdbe30ed0bd25535ce8956c42d2ebdc9a1
surajmurtadak <surajmurtadak@gmail.com>
2023-06-21 16:15:39 +0530

    xylophone developed

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,95 +13,101 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  void playSound(int num) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('sound$num.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Expanded(
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () => playSound(1),
                   style: TextButton.styleFrom(
                     padding: EdgeInsets.zero,
-                    backgroundColor: Colors.black38,
+                    backgroundColor: Colors.red,
+                    shape: const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.zero),
                   ),
-                  child: const SizedBox(
-                    width: double.infinity,
-                  ),
+                  child: const SizedBox(),
                 ),
               ),
               Expanded(
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () => playSound(2),
                   style: TextButton.styleFrom(
                     padding: EdgeInsets.zero,
-                    backgroundColor: Colors.black,
+                    backgroundColor: Colors.orange,
+                    shape: const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.zero),
                   ),
-                  child: const SizedBox(
-                    width: double.infinity,
-                  ),
+                  child: const SizedBox(),
                 ),
               ),
               Expanded(
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () => playSound(3),
                   style: TextButton.styleFrom(
                     padding: EdgeInsets.zero,
-                    backgroundColor: Colors.black38,
+                    backgroundColor: Colors.yellow,
+                    shape: const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.zero),
                   ),
-                  child: const SizedBox(
-                    width: double.infinity,
-                  ),
+                  child: const SizedBox(),
                 ),
               ),
               Expanded(
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () => playSound(4),
                   style: TextButton.styleFrom(
                     padding: EdgeInsets.zero,
-                    backgroundColor: Colors.black38,
+                    backgroundColor: Colors.green,
+                    shape: const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.zero),
                   ),
-                  child: const SizedBox(
-                    width: double.infinity,
-                  ),
+                  child: const SizedBox(),
                 ),
               ),
               Expanded(
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () => playSound(5),
                   style: TextButton.styleFrom(
                     padding: EdgeInsets.zero,
-                    backgroundColor: Colors.black38,
+                    backgroundColor: Colors.blue,
+                    shape: const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.zero),
                   ),
-                  child: const SizedBox(
-                    width: double.infinity,
-                  ),
+                  child: const SizedBox(),
                 ),
               ),
               Expanded(
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () => playSound(6),
                   style: TextButton.styleFrom(
                     padding: EdgeInsets.zero,
-                    backgroundColor: Colors.black38,
+                    backgroundColor: Colors.indigo,
+                    shape: const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.zero),
                   ),
-                  child: const SizedBox(
-                    width: double.infinity,
-                  ),
+                  child: const SizedBox(),
                 ),
               ),
               Expanded(
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () => playSound(7),
                   style: TextButton.styleFrom(
                     padding: EdgeInsets.zero,
-                    backgroundColor: Colors.black38,
+                    backgroundColor: const Color(0xff9400D3),
+                    shape: const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.zero),
                   ),
-                  child: const SizedBox(
-                    width: double.infinity,
-                  ),
+                  child: const SizedBox(),
                 ),
               ),
             ],
